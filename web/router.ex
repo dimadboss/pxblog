4c4a01998a0820f52263c6481d48dbd069cdf714
dimadboss <ds-drozdov@yandex.ru>
2022-04-17 14:02:13 +0300

    add session

--- web/router.ex
+++ web/router.ex
@@ -2,23 +2,25 @@ defmodule Pxblog.Router do
   use Pxblog.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", Pxblog do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
-    resources "/posts", PostController
-    resources "/users", UserController
+    get("/", PageController, :index)
+    resources("/posts", PostController)
+    resources("/users", UserController)
+    resources("/sessions", SessionController, only: [:new, :create])
   end
 
   # Other scopes may use custom stacks.

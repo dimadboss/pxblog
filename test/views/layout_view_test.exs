609eded303ad1c7e886778246e0d37680569b1e6
dimadboss <ds-drozdov@yandex.ru>
2022-04-24 12:42:53 +0300

    rework layout test after adding roles

--- test/views/layout_view_test.exs
+++ test/views/layout_view_test.exs
@@ -2,28 +2,35 @@ defmodule Pxblog.LayoutViewTest do
   use Pxblog.ConnCase, async: true
   alias Pxblog.LayoutView
   alias Pxblog.User
+  alias Pxblog.TestHelper
 
   setup do
-    User.changeset(%User{}, %{
-      username: "test",
-      password: "test",
-      password_confirmation: "test",
-      email: "[email protected]"
-    })
-    |> Repo.insert()
+    {:ok, role} = TestHelper.create_role(%{name: "User Role", admin: false})
 
-    {:ok, conn: build_conn()}
+    {:ok, user} =
+      TestHelper.create_user(role, %{
+        email: "test@test.com",
+        username: "testuser",
+        password: "test",
+        password_confirmation: "test"
+      })
+
+    {:ok, conn: build_conn(), user: user}
   end
 
-  test "current user returns the user in the session", %{conn: conn} do
-    conn = post(conn, session_path(conn, :create), user: %{username: "test", password: "test"})
+  test "current user returns the user in the session", %{conn: conn, user: user} do
+    conn =
+      post(conn, session_path(conn, :create),
+        user: %{username: user.username, password: user.password}
+      )
+
     assert LayoutView.current_user(conn)
   end
 
   test "current user returns nothing if there is no user in the session (delete session)", %{
-    conn: conn
+    conn: conn,
+    user: user
   } do
-    user = Repo.get_by(User, %{username: "test"})
     conn = delete(conn, session_path(conn, :delete, user))
     refute LayoutView.current_user(conn)
   end

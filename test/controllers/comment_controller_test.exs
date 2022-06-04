4791ced57e540fd0821735701380fdeb60f67d67
dimadboss <ds-drozdov@yandex.ru>
2022-06-04 22:46:28 +0300

    add authorize check for comments

--- test/controllers/comment_controller_test.exs
+++ test/controllers/comment_controller_test.exs
@@ -26,16 +26,31 @@ defmodule Pxblog.CommentControllerTest do
     assert html_response(conn, 200) =~ "Oops, something went wrong"
   end
 
-  test "deletes the comment", %{conn: conn, post: post, comment: comment} do
-    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
-    assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
-    refute Repo.get(Comment, comment.id)
-  end
-
   defp login_user(conn, user) do
     post(conn, session_path(conn, :create),
       user: %{username: user.username, password: user.password}
     )
+  end
+
+  test "deletes the comment when logged in as an authorized user", %{
+    conn: conn,
+    user: user,
+    post: post,
+    comment: comment
+  } do
+    conn = login_user(conn, user) |> delete(post_comment_path(conn, :delete, post, comment))
+    assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
+    refute Repo.get(Comment, comment.id)
+  end
+
+  test "does not delete the comment when not logged in as an authorized user", %{
+    conn: conn,
+    post: post,
+    comment: comment
+  } do
+    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+    assert redirected_to(conn) == page_path(conn, :index)
+    assert Repo.get(Comment, comment.id)
   end
 
   test "updates chosen resource and redirects when data is valid and logged in as the author", %{
@@ -50,5 +65,17 @@ defmodule Pxblog.CommentControllerTest do
 
     assert redirected_to(conn) == user_post_path(conn, :show, user, post)
     assert Repo.get_by(Comment, %{id: comment.id, approved: true})
+  end
+
+  test "does not update the comment when not logged in as an authorized user", %{
+    conn: conn,
+    post: post,
+    comment: comment
+  } do
+    conn =
+      put(conn, post_comment_path(conn, :update, post, comment), comment: %{"approved" => true})
+
+    assert redirected_to(conn) == page_path(conn, :index)
+    refute Repo.get_by(Comment, %{id: comment.id, approved: true})
   end
 end
